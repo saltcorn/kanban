b248c5574e6c6c284d43f535e7e1f2a74167cd83
Tom Nielsen <tanielsen@gmail.com>
2020-04-28 10:59:12 +0100

    kanban display

--- kanban.js
+++ kanban.js
@@ -4,7 +4,7 @@ const Form = require("saltcorn-data/models/form");
 const View = require("saltcorn-data/models/view");
 const Workflow = require("saltcorn-data/models/workflow");
 
-const { textarea, text } = require("saltcorn-markup/tags");
+const { text, div, h3, style } = require("saltcorn-markup/tags");
 
 const configuration_workflow = () =>
   new Workflow({
@@ -12,19 +12,20 @@ const configuration_workflow = () =>
       {
         name: "views",
         form: async context => {
+          console.log({ context });
+
           const table = await Table.findOne({ id: context.table_id });
           const fields = await table.getFields();
 
           const show_views = await View.find_table_views_where(
             context.table_id,
             ({ state_fields, viewtemplate, viewrow }) =>
-              viewtemplate.runMany &&
-              viewrow.name !== context.viewname &&
-              state_fields.some(sf => sf.name === "id")
+              viewtemplate.runMany && viewrow.name !== context.viewname
           );
           const show_view_opts = show_views.map(v => v.name);
-
+          console.log({ show_views });
           return new Form({
+            blurb: JSON.stringify(show_views),
             fields: [
               {
                 name: "show_view",
@@ -60,7 +61,20 @@ const get_state_fields = async (table_id, viewname, { show_view }) => {
   });
 };
 
-const nub = xs => [...new Set(xs)];
+//https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
+function groupBy(list, keyGetter) {
+  var map = {};
+  list.forEach(item => {
+    const key = keyGetter(item);
+    const collection = map[key];
+    if (!collection) {
+      map[key] = [item];
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
 
 const run = async (
   table_id,
@@ -70,9 +84,27 @@ const run = async (
   extraArgs
 ) => {
   const sview = await View.findOne({ name: show_view });
-  const sresp = await sview.runMany(state, extraArgs);
-  var cols = new Set(sresp.map(({ row }) => row[column_field]));
-  return sresp.map(r => div(r.html) + hr());
+  const sresps = await sview.runMany(state, extraArgs);
+  var cols = groupBy(sresps, ({ row }) => row[column_field]);
+  const col_divs = Object.entries(cols).map(([k, vs]) =>
+    div(
+      { class: "kancol" },
+      h3(text(k)),
+      vs.map(({ row, html }) => div({ class: "kancard" }, html))
+    )
+  );
+  return (
+    div({ class: "d-flex" }, col_divs) +
+    style(`
+         .kancol { border: 1px solid black;
+                   padding:2px ; margin:2px;
+                  }
+         .kancard { border: 1px solid blue;  
+                    padding:2px;
+                    margin:2px;
+                  }
+         `)
+  );
 };
 
 module.exports = {
